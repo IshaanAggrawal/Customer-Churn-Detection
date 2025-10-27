fb4469b64b920c4a0bc3cc99d865b3c6ce04296a
Ishaan Aggrawal <ishaanaggrawal101@gmail.com>
2025-10-27 21:48:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def predict():
 
     except Exception as e:
         return f"Error: {str(e)}"
-
+        
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
